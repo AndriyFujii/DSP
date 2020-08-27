2c04196fa97edfa1eb583906a88b0402d950863c
Andriy Fuji Pendloski <afp@edu.univali.br>
2020-08-27 10:27:31 -0300

    Aula 2 concluída

--- Aula 2/sinais_discretos.py
+++ Aula 2/sinais_discretos.py
@@ -28,7 +28,7 @@ def unit(t):
     return z
     
 
-plt.figure("Gráficos",figsize=(15,8))
+plt.figure("Gráficos",figsize=(15,12))
 
 # Plotando os gráficos
 x = np.linspace(-1, 8, 10)
@@ -37,7 +37,7 @@ x = np.linspace(-1, 8, 10)
 #pulso = signal.unit_impulse(10, idx = 1)
 pulso = unit(x)
 
-plt.subplot(411)
+plt.subplot(611)
 plt.title("Impulso unitário")
 plt.xlabel("n")
 plt.ylabel("x[n]")
@@ -47,9 +47,10 @@ plt.xticks(np.arange(-1, 8.1, 1))
 plt.yticks(np.arange(0, 1.1, 1))
 
 # Degrau unitário
+
 degrau = step(x)
 
-plt.subplot(412)
+plt.subplot(612)
 plt.title("Degrau unitário")
 plt.xlabel("n")
 plt.ylabel("x[n]")
@@ -59,28 +60,55 @@ plt.xticks(np.arange(-1, 8.1, 1))
 plt.yticks(np.arange(0, 1.1, 1))
 
 # Seno
-seno = np.sin(x)
+x2 = np.linspace(-1, 30, 32)
+fo = 100
+Fs = 8000
+seno = np.sin(2*np.pi*x2*fo/Fs)
 
-plt.subplot(413)
-plt.title("Sequência sinusoidal")
+plt.subplot(613)
+plt.title("Sequência sinusoidal Fo = 100Hz e Fs = 8kHz")
 plt.xlabel("n")
 plt.ylabel("x[n]")
 plt.grid(1)
-plt.stem(x, seno)
-plt.xticks(np.arange(-1, 8.1, 1))
+plt.stem(x2, seno)
+plt.xticks(np.arange(-1, 30.1, 1))
 plt.yticks(np.arange(0, 1.1, 1))
 
-# Exponencial
-exponencial =  0.5 ** x
+# Exponencial1
+exponencial =  0.5 ** x2
 
-plt.subplot(414)
+plt.subplot(614)
 plt.title("Sequência exponencial A=1 e a=0,5")
 plt.xlabel("n")
 plt.ylabel("x[n]")
 plt.grid(1)
-plt.stem(x, exponencial)
-plt.xticks(np.arange(-1, 8.1, 1))
+plt.stem(x2, exponencial)
+plt.xticks(np.arange(-1, 30.1, 1))
 plt.yticks(np.arange(0, 2.1, 1))
+
+# Exponencial2
+exponencial2 =  -0.5 ** x2
+
+plt.subplot(615)
+plt.title("Sequência exponencial A=1 e a=-0,5")
+plt.xlabel("n")
+plt.ylabel("x[n]")
+plt.grid(1)
+plt.stem(x2, exponencial2)
+plt.xticks(np.arange(-1, 30.1, 1))
+plt.yticks(np.arange(-2, 0.1, 1))
+
+# Exponencial3
+exponencial2 =  2 ** x
+
+plt.subplot(616)
+plt.title("Sequência exponencial A=1 e a=2")
+plt.xlabel("n")
+plt.ylabel("x[n]")
+plt.grid(1)
+plt.stem(x, exponencial2)
+plt.xticks(np.arange(-1, 8.1, 1))
+plt.yticks(np.arange(0, 256.1, 32))
 
 
 
