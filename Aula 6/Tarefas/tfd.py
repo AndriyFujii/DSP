dae3972b775674ee6698ea5b160384a14adddc37
Andriy Fuji Pendloski <afp@edu.univali.br>
2020-10-06 08:29:02 -0300

    Código tfd aula 6

---
+++ Aula 6/Tarefas/tfd.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Andriy
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+a = 0.5
+w = np.arange(-1*np.pi, 1*np.pi, np.pi/100)
+
+num = 1
+den = 1 - a * np.exp(-1j*w) # (complex(0, -1)*w) # 1-a^(-j*w) 
+x = num/den
+
+mod_x = np.absolute(x)
+fase_x = np.angle(den)
+
+plt.figure("Gráficos",figsize=(15,12))
+
+# Plotando os gráficos
+x = np.linspace(-1, 8, 10)
+
+
+plt.subplot(211)
+plt.title("Mod x")
+plt.xlabel("n")
+plt.ylabel("x[n]")
+plt.grid(1)
+plt.plot(mod_x)
+plt.xticks(np.arange(-1, 8.1, 1))
+plt.yticks(np.arange(0, 1.1, 1))
+
+
+plt.subplot(212)
+plt.title("Fase X")
+plt.xlabel("n")
+plt.ylabel("x[n]")
+plt.grid(1)
+plt.plot(mod_x)
+plt.xticks(np.arange(-1, 8.1, 1))
+plt.yticks(np.arange(0, 1.1, 1))
+
+
+
+plt.tight_layout()
+
+
+
+
+
+# Salvando os gráficos
+plt.savefig("graficos_sinais.png", format="png")
+
+plt.show()
+
